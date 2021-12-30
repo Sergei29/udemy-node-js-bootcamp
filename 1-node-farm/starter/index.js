1d7bca95b7ef57a62bd417cf4a7f55162158a8cd
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2021-12-30 19:49:56 +0200

    installed additional packages

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,32 +1,48 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // Synchronous blocking code:
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
 const textOut = `This is what we know about the avocado: ${textIn}. \nCreated at: ${new Date()}`;
 
-fs.writeFileSync('./txt/output.txt', textOut);
+fs.writeFileSync("./txt/output.txt", textOut);
 
 // async non-blocking code:
 
 const handleError = (error) => {
   console.error(error.message);
-  throw new Error(error.message)
-}
+  throw new Error(error.message);
+};
 
-fs.readFile(`${__dirname}/txt/start.txt`,{encoding: 'utf-8'} ,(error1, data1) => {
-  if (error1) return handleError(error1);
-  fs.readFile(`${__dirname}/txt/${data1}.txt`,{encoding: 'utf-8'} , (error2, data2) => {
-    if (error2) return handleError(error2);
-    fs.readFile(`${__dirname}/txt/append.txt`,{encoding: 'utf-8'} , (error3, data3) => {
-      if (error3) return handleError(error3);
-      const strAggregated = `${data2}\n${data3}`;
-      fs.writeFile(`${__dirname}/txt/final.txt`, strAggregated, {encoding: 'utf-8'}, (errorWrite) => {
-        if (errorWrite) return handleError(errorWrite);
-        console.log('File Written OK!');
-        
-      })
-    })
-  })
-})
+fs.readFile(
+  `${__dirname}/txt/start.txt`,
+  { encoding: "utf-8" },
+  (error1, data1) => {
+    if (error1) return handleError(error1);
+    fs.readFile(
+      `${__dirname}/txt/${data1}.txt`,
+      { encoding: "utf-8" },
+      (error2, data2) => {
+        if (error2) return handleError(error2);
+        fs.readFile(
+          `${__dirname}/txt/append.txt`,
+          { encoding: "utf-8" },
+          (error3, data3) => {
+            if (error3) return handleError(error3);
+            const strAggregated = `${data2}\n${data3}`;
+            fs.writeFile(
+              `${__dirname}/txt/final.txt`,
+              strAggregated,
+              { encoding: "utf-8" },
+              (errorWrite) => {
+                if (errorWrite) return handleError(errorWrite);
+                console.log("File Written OK!");
+              }
+            );
+          }
+        );
+      }
+    );
+  }
+);
